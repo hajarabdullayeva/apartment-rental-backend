ddd0ad63b839c78c4e3d4b6925dc4383cef3ad9a
HajarAb <hadjar.abdullayeva@gmail.com>
2022-08-21 17:41:34 +0400

    done

--- src/app.ts
+++ src/app.ts
@@ -9,12 +9,7 @@ const app = express();
 
 const server = http.createServer(app);
 
-app.use(
-  cors({
-    origin: "http://192.168.31.108:3000",
-    methods: ["GET", "POST", "DELETE", "PUT"],
-  })
-);
+app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
